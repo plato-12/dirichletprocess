b5e08487db2d715d95d8789e248af3e7740e7074
Priyanshu Tiwari <priyanshut21@iitk.ac.in>
2025-06-22 19:56:04 +0530

    weibull mcmc_runner implemented

--- src/mixing_distribution_base.cpp
+++ src/mixing_distribution_base.cpp
@@ -19,8 +19,7 @@ std::unique_ptr<MixingDistribution> MixingDistribution::create(
     double beta0 = Rcpp::as<double>(params["beta0"]);
     return std::unique_ptr<MixingDistribution>(
       new GaussianMixing(mu0, kappa0, alpha0, beta0));
-  }
-  else if (type == "beta") {
+  } else if (type == "beta") {
     double alpha0 = Rcpp::as<double>(params["alpha0"]);
     double beta0 = Rcpp::as<double>(params["beta0"]);
     double maxT = params.containsElementNamed("maxT") ?
@@ -28,8 +27,7 @@ std::unique_ptr<MixingDistribution> MixingDistribution::create(
 
     return std::unique_ptr<MixingDistribution>(
       new BetaMixing(alpha0, beta0, maxT));
-  }
-  else if (type == "mvnormal") {
+  } else if (type == "mvnormal") {
     arma::vec mu0 = Rcpp::as<arma::vec>(params["mu0"]);
     double kappa0 = Rcpp::as<double>(params["kappa0"]);
     arma::mat Lambda = Rcpp::as<arma::mat>(params["Lambda"]);
@@ -37,8 +35,7 @@ std::unique_ptr<MixingDistribution> MixingDistribution::create(
 
     return std::unique_ptr<MixingDistribution>(
       new MVNormalMixing(mu0, kappa0, Lambda, nu));
-  }
-  else if (type == "weibull") {
+  } else if (type == "weibull") {
     double phi = Rcpp::as<double>(params["phi"]);
     double alpha0 = Rcpp::as<double>(params["alpha0"]);
     double beta0 = Rcpp::as<double>(params["beta0"]);
@@ -63,13 +60,7 @@ std::unique_ptr<MixingDistribution> MixingDistribution::create(
       new WeibullMixing(phi, alpha0, beta0, hyper_a1, hyper_a2,
                         hyper_b1, hyper_b2, mh_step_alpha, mh_draws));
   }
-  else if (type == "exponential") {
-    double alpha0 = Rcpp::as<double>(params["alpha0"]);
-    double beta0 = Rcpp::as<double>(params["beta0"]);
-
-    return std::unique_ptr<MixingDistribution>(
-      new ExponentialMixing(alpha0, beta0));
-  }
+  // Remove any ExponentialMixing case that might be here
 
   Rcpp::stop("Unknown mixing distribution type: " + type);
 }
